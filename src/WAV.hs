e5015a346f142ad34866949b7d86a1f5d0545e6c
Y.Naito <tsukudani710@gmail.com>
2025-03-11 21:34:12 +0900

    rm: delete unused functions

--- src/WAV.hs
+++ src/WAV.hs
@@ -2,14 +2,11 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module WAV
-  ( playWAV,
-    playWAVFile,
-    playWavOpenAL,
+  ( playWavOpenAL,
   )
 where
 
 import Control.Concurrent (threadDelay)
-import Control.Exception (SomeException, catch)
 import Control.Monad (when)
 import Data.Bits (Bits (shiftL))
 import qualified Data.ByteString as BS
@@ -40,48 +37,6 @@ import Sound.OpenAL
     queueBuffers,
     sourceState,
   )
-import System.IO (hClose)
-import System.IO.Temp (withSystemTempFile)
-import qualified System.Info as SysInfo
-import System.Process.ByteString.Lazy (readProcessWithExitCode)
-
--- | Save WAV data to a temporary file and play it
-playWAV :: BL.ByteString -> IO Bool
-playWAV wavData = do
-  withSystemTempFile "temp.wav" $ \filePath handle -> do
-    BL.hPut handle wavData
-    hClose handle
-    playWAVFile filePath `catch` handlePlayError
-  where
-    handlePlayError :: SomeException -> IO Bool
-    handlePlayError _ = do
-      BL.putStr "Error occurred while playing WAV file\n"
-      return False
-
--- | Play a WAV file
-playWAVFile :: FilePath -> IO Bool
-playWAVFile filePath = do
-  let cmd = case os of
-        "windows" -> "powershell"
-        "darwin" -> "afplay"
-        _ -> "aplay" -- Linux
-      args = case os of
-        "windows" -> ["-c", "(New-Object System.Media.SoundPlayer '" ++ filePath ++ "').PlaySync()"]
-        _ -> [filePath]
-
-  (exitCode, _, stderr) <- readProcessWithExitCode cmd args BL.empty
-  if BL.null stderr
-    then return True
-    else do
-      BL.putStr ("Error: " <> stderr <> "\n")
-      return False
-
--- | Get current OS name
-os :: String
-os = case SysInfo.os of
-  "mingw32" -> "windows"
-  "darwin" -> "darwin"
-  _ -> "linux"
 
 -- | Play WAV data using OpenAL directly from ByteString without temporary files
 playWavOpenAL :: BL.ByteString -> IO Bool
